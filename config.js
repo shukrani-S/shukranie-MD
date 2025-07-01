fd058e93e20d0479e43bd53e8a14f7bdfb4a1105
shukrani-S <shukranikimath@gmail.com>
2025-07-01 23:12:15 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,9 @@
+require('dotenv').config();
 
+module.exports = {
+  ownerName: process.env.OWNER_NAME || 'SHUKRANI R RAY',
+  ownerJid: process.env.NUMERO_OWNER || '255773350309@s.whatsapp.net',
+  botName: process.env.BOT_NAME || 'SHUKRANI-MD',
+  imageMenu: process.env.IMAGE_MENU || '',
+  prefix: process.env.PREFIX || '.', // unaweza ignore prefix kama hautumii
+};
