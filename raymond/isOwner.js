0fb5096cc2dd06744b877b0f5f9a2469a8acff0a
shukrani-S <shukranikimath@gmail.com>
2025-07-02 22:00:48 +0300

    Create isOwner.js

---
+++ raymond/isOwner.js
@@ -0,0 +1,8 @@
+// ðŸ“ utils/isOwner.js
+const control = require('../control');
+
+function isOwner(m) {
+  return m.key.fromMe; // This checks if the sender is the same number that scanned the QR/started the session
+}
+
+module.exports = isOwner;
