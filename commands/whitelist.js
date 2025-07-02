35e7d670736732348eefdd26c22b83bd27628435
shukrani-S <shukranikimath@gmail.com>
2025-07-02 23:05:12 +0300

    Update whitelist.js

--- commands/whitelist.js
+++ commands/whitelist.js
@@ -1,6 +1,4 @@
-// 📁 commands/whitelist.js const { getSetting, setSetting } = require('../database/settings'); const isAdmin = require('../raymond/isAdmin');
-
-// In-memory store for whitelisted numbers const whitelist = new Set();
+// 📁 commands/whitelist.js const db = require('../database/db'); const isAdmin = require('../raymond/isAdmin');
 
 module.exports = { name: 'whitelist', description: 'Allow or disallow a number to send links (no kick).',
 
@@ -19,7 +17,7 @@ if (!number || number.length < 9) {
 const jid = number + '@s.whatsapp.net';
 
 if (isRemove) {
-  whitelist.delete(jid);
+  await db.query('DELETE FROM whitelist WHERE jid = $1', [jid]);
   await sock.sendMessage(m.key.remoteJid, {
     react: { text: '😈', key: m.key }
   });
@@ -27,7 +25,7 @@ if (isRemove) {
     text: `❌ ${number} has been removed from the whitelist. Links will be blocked.\nPower by SHUKRANI`
   }, { quoted: m });
 } else {
-  whitelist.add(jid);
+  await db.query('INSERT INTO whitelist (jid) VALUES ($1) ON CONFLICT (jid) DO NOTHING', [jid]);
   await sock.sendMessage(m.key.remoteJid, {
     react: { text: '😈', key: m.key }
   });
@@ -38,6 +36,6 @@ if (isRemove) {
 
 },
 
-isWhitelisted(jid) { return whitelist.has(jid); } };
+async isWhitelisted(jid) { const res = await db.query('SELECT 1 FROM whitelist WHERE jid = $1 LIMIT 1', [jid]); return res.rowCount > 0; } };
 
-  
+    
